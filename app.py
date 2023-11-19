26d47b9262dce401a8232e2af19421943dd63bee
Will Isles <isles.william@gmail.com>
2023-11-19 16:33:22 +0100

    add screenshotting

--- app.py
+++ app.py
@@ -1,45 +1,45 @@
-import os
 import streamlit as st
 from todoist_api_python.api import TodoistAPI
-
+import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Load Todoist List
 TODOIST_API_KEY = os.getenv('TODOIST_API_KEY')
 api = TodoistAPI(TODOIST_API_KEY)
 
-def load_tasks():
+BACKGROUND_COLOR = 'white'
+COLOR = 'black'
+
+def set_page_container_style(
+        padding_top: int = 1
+    ):
+        st.markdown(
+            f'''
+            <style>
+                .block-container {{
+                    padding-top: {padding_top}rem;
+                }}
+            </style>
+            ''',
+            unsafe_allow_html=True,
+        )
+
+
+# Fetch all tasks
+def fetch_tasks():
     try:
-        st.session_state['tasks'] = api.get_tasks()
+        tasks = api.get_tasks()
     except Exception as error:
         print(error)
+    return tasks
 
-st.set_page_config(page_title="Streamlit App", page_icon=":smiley:", layout="wide")
+st.set_page_config(page_title=None, page_icon=None, layout="centered", initial_sidebar_state="collapsed", menu_items=None)
+set_page_container_style()
 
-
-# Task Layout
-def layoutTask(task):
-    with st.container():
-        st.header(f"Content: {task.content}")
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            st.write(f"Labels: {task.labels}")
-        with col2:
-            st.write(f"Priority: {task.priority}")
-        with col3:
-            if task.due is not None:
-                st.write(f"Due: {task.due}")
-
-# Page Layout
-if 'tasks' not in st.session_state:
-    load_tasks()
-
-with st.container():
-    st.subheader("Todo List")
-    st.title("Today's Items")
-
-    for task in st.session_state['tasks']:
-        layoutTask(task)
-
-
+# Display tasks
+st.header('Todoist Tasks')
+tasks = fetch_tasks()
+for task in tasks:
+    st.write(f"Task: {task.content}, Label: {task.labels}, Priority: {task.priority}")
